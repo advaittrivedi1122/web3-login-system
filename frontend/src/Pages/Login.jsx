c2a7d04e072d433c325e70cc56177ffc86db507d
Advait Trivedi <trivediadvait0@gmail.com>
2023-06-09 01:51:38 +0530

    basic ui for login

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+import {React, useState} from "react";
+import "../Styles/Login.css";
+import "../Styles/General.css";
+
+function Login() {
+  return (
+    <div className="parent-container">
+      <div className="glass-card">
+        <p>Login</p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
